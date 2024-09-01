e0989ec406adcc5e91e451275cc9ed2b3ba361d4
Samarth375-hub <kunala-mca23@cs.du.ac.in>
2024-09-01 19:42:48 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def Addition(a,b):
+   
+    return a+b
+
+
+def Multiplication(a,b):
+     
+     return a*b
+
+
+def Divide(a,b):
+    
+     return a/b
+
+
+def Subtraction(a,b):
+     
+     return a-b
+
+
---
+++ constants.py
@@ -0,0 +1,2 @@
+speedoflight = 299792458  
+PI = 3.14
